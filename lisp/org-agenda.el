3814ce18157eae492cdfbb66cfff5ce17f59a606
Ivan Malison <IvanMalison@gmail.com>
2024-12-31 20:22:53 -0700

    Dont show deadline is scheduled

--- lisp/org-agenda.el
+++ lisp/org-agenda.el
@@ -6411,10 +6411,16 @@ specification like [h]h:mm."
 			    org-deadline-warning-days))
 		      ;; Set pre-warning to deadline.
 		      (t 0))))
-	          (warning-days (min max-warning-days (org-get-wdays s))))
+	          (warning-days (min max-warning-days (org-get-wdays s)))
+                  (habitp (and (fboundp 'org-is-habit-p)
+                               (string= "habit" (org-element-property :STYLE el)))))
 	     (cond
 	      ;; Only display deadlines at their base date, at future
 	      ;; repeat occurrences or in today agenda.
+              ((and org-agenda-skip-deadline-prewarning-if-scheduled
+                    habitp
+                    is-scheduled)
+               (throw :skip nil))
 	      ((= current deadline) nil)
 	      ((= current repeat) nil)
 	      ((not today?) (throw :skip nil))
